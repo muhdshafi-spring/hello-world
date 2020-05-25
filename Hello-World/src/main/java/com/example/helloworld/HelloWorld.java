55dc4c9eff453a4430af125d05302987a4298410
Shafi <shafikulambil@gmail.com>
2020-05-25 21:25:22 +0530

    .

---
+++ Hello-World/src/main/java/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+	public void sayHello() {
+		System.out.println("*************  Hello World!!!  ***************");
+	}
+}
